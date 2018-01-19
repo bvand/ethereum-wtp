584d83e4228edb71a7b1bfcd952659fbf52d0bbc
Brian Vander Schaaf <bvs330@gmail.com>
2018-01-18 23:33:42 -0500

    switch to await & async

--- test/winThePot.js
+++ test/winThePot.js
@@ -1,11 +1,17 @@
 const WinThePot = artifacts.require("WinThePot");
 
 contract("WinThePot", function(accounts) {
-    it("should initialize contract with expected owner", function() {
-        return WinThePot.deployed().then(function(instance) {
-            return instance.owner();
-        }).then(function(owner) {
-            assert.equal(owner, accounts[0]);
-        });
+    let contract;
+
+    const account = accounts[1];
+    const offerName = "Purchase discount";
+
+    beforeEach(async function () {
+        contract = await WinThePot.new();
+    });
+
+    it("should initialize contract with expected owner", async function() {
+        const owner = await contract.owner();
+        assert.equal(owner, accounts[0]);
     });
 });
